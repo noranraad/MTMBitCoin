b172923ed68f103320126f2b8b2bbeadec02e4ce
noranraad <noran.raad@campus.technion.ac.il>
2026-05-24 11:35:53 +0300

    implement the first and second func'

--- Transaction.cpp
+++ Transaction.cpp
@@ -1,3 +1,18 @@
 //
 // Created by Noran on 5/24/2026.
 //
+#include "Transaction.h"
+#include "Utilities.h"
+
+void TransactionDumpInfo(const Transaction& transaction, ofstream& file) {
+    file << "Sender Name:" << " " << transaction.sender;
+    file << "Receiver Name:" << " " << transaction.receiver;
+    file << "Transaction Value:" << " " << transaction.value;
+}
+
+string TransactionHashedMessage(const Transaction& transaction) {
+    int key = static_cast<int>(transaction.value);
+    return hash(key,transaction.sender,transaction.receiver);
+}
+
+
